84662162d8c16d398c7144f47f0cd1eaafdff077
Victor Martinez <vicplusplus@proton.me>
2023-04-10 10:46:04 -0400

    Create outline shader, add a 1px outline space to all sprites

--- Assets/Scripts/Characters/CharacterController.cs
+++ Assets/Scripts/Characters/CharacterController.cs
@@ -3,10 +3,6 @@ using UnityEngine.InputSystem;
 
 public class CharacterController : MonoBehaviour, Controls.IInGameActions
 {
-    private Vector2 _moveDirection;
-    private bool _isJumping;
-    private bool _isPossessing;
-    private bool _isInteracting;
     public Character Character;
     private Controls _controls;
 
